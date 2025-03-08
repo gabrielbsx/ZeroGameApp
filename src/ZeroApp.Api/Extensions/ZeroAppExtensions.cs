327727bafd71d2ef6deb09bafed56d01ce2a9fee
Gabriel.Barbosa <gabrielturing@gmail.com>
2025-03-08 14:16:14 -0300

    chore: add status 200 for error (it's not a good choice but hz use this approach to handle messages)

--- src/ZeroApp.Api/Extensions/ZeroAppExtensions.cs
+++ src/ZeroApp.Api/Extensions/ZeroAppExtensions.cs
@@ -1,5 +1,6 @@
 using FluentValidation;
 using MediatR;
+using ZeroApp.Api.Behaviors;
 using ZeroApp.Api.Middlewares;
 using ZeroApp.Core.Features.Commands.CreateCharacter;
 using ZeroApp.Core.Features.Commands.RegisterUser;
@@ -14,6 +15,8 @@ public static class ZeroAppExtensions
 
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(assemblies));
         services.AddValidatorsFromAssemblies(assemblies);
+        
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
 
         services.AddScoped<IRequestHandler<RegisterUserRequest, RegisterUserResponse>, RegisterUserCommand>();
         services.AddScoped<IRequestHandler<CreateCharacterRequest, CreateCharacterResponse>, CreateCharacterCommand>();
@@ -22,6 +25,7 @@ public static class ZeroAppExtensions
     public static void Configure(this IApplicationBuilder app)
     {
         app.UseMiddleware<ActionValidationMiddleware>();
+        app.UseMiddleware<ErrorBoundaryMiddleware>();
         app.UseRouting();
         app.UseEndpoints(endpoints => endpoints.MapControllers());
     }
--- src/ZeroApp.Api/Middlewares/ErrorBoundaryMiddleware.cs
+++ src/ZeroApp.Api/Middlewares/ErrorBoundaryMiddleware.cs
@@ -1,6 +1,41 @@
+using System.Net;
+using System.Text.Json;
+
 namespace ZeroApp.Api.Middlewares;
 
 public class ErrorBoundaryMiddleware
 {
+    private readonly RequestDelegate _next;
     
+    public ErrorBoundaryMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+    
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+    
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        var response = new {
+            data = (object)null!,
+            error = exception.Message
+        };
+        
+        var result = JsonSerializer.Serialize(response);
+        
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.OK;
+        
+        return context.Response.WriteAsync(result);
+    }
 }
